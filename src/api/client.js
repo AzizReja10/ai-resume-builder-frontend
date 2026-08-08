1da918291d46521897d9372ba172ac59e99bab92
AzizReja10 <rejaaziz686@gmail.com>
2026-08-09 01:10:14 +0530

    new url

--- src/api/client.js
+++ src/api/client.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const client=axios.create({
-    baseURL:"http://localhost:8000"
+    baseURL:"https://ai-resume-builder-backend-m3v3.onrender.com"
 });
 export function setAuthToken(token) {
     if(token)
